afc5f50a1ad17ce06d1b3b072b8214fee5269c46
Danielle Jasper <dnj547@live.com>
2019-03-19 00:37:31 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -38,7 +38,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  puts "Please enter a song name:"
+  puts "Please enter a song name"
   user_input = gets.chomp
   if my_songs.keys.include?(user_input)
     puts "Now playing #{user_input}"
